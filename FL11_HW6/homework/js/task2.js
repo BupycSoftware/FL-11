a47f9e515bd5acfeef44a04509be54ab09f6237e
Dmitriy Khomiak <44303422+BupycSoftware@users.noreply.github.com>
2019-07-07 16:12:58 +0300

    Add files via upload

--- FL11_HW6/homework/js/task2.js
+++ FL11_HW6/homework/js/task2.js
@@ -2,7 +2,7 @@ let a = +prompt('Enter a= ');
 let b = +prompt('Enter b= ');
 let c = +prompt('Enter c= ');
 
-if (a + b >= c && a + c >= b && b + c >= a) {
+if (a + b > c && a + c > b && b + c > a) {
     if (a === b && a === c && b === c) {
         console.log('Eequivalent triangle');
     } else if (a === b || a === c || b === c) {
