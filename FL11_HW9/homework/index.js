72f72a5064bfc04940b59d228846e8d3ef4fe361
Dmitriy Khomiak <44303422+BupycSoftware@users.noreply.github.com>
2019-07-16 17:47:09 +0300

    area51 confirmed

--- FL11_HW9/homework/index.js
+++ FL11_HW9/homework/index.js
@@ -1,14 +1,140 @@
 function getNumbers(string) {
     let array = new Array();
-    value = +string;
-    console.log(value);
     for (let i = 0; i < string.length; i++) {
-        console.log(typeof string[i]);
-        if (typeof string[i] === Number) {
-            array.push(string[i]);
+        if (parseFloat(string[i])) {
+            let test = parseFloat(string[i]);
+            array.push(test);
         }
     }
     return array;
 }
 
-console.log(getNumbers('n1um3ber95'));
+function findTypes() {
+    const obj = new Object();
+    for (let i = 0; i < arguments.length; i++) {
+        let type = typeof arguments[i];
+        obj[type] ? obj[type]++ : obj[type] = 1;
+    }
+    return obj;
+}
+
+function executeforEach(received_array, received_function) {
+    for (let i = 0; i < received_array.length; i++) {
+        received_function(received_array[i]);
+    }
+}
+
+function mapArray(received_array, received_function) {
+    let array = [];
+    executeforEach(received_array, function(el) {
+        array.push(received_function(el));
+    });
+    return array;
+}
+
+function filterArray(received_array, received_function) {
+    let array = [];
+    executeforEach(received_array, function(el) {
+        const status = received_function(el);
+        if (status) {
+            array.push(el);
+        }
+    });
+    return array;
+}
+
+function showFormattedDate(received_date) {
+    const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+    let month_index = received_date.getMonth();
+    let result = 'Date: ' + months[month_index] + ' ' + received_date.getDate() + ' ' + received_date.getFullYear();
+    return result;
+}
+
+function canConvertToDate(string) {
+    let received_date = new Date(string);
+    if (Date.parse(received_date)) {
+        return true;
+    }
+    return false;
+}
+
+function daysBetween(first_date, second_date) {
+    const thousand = 1000;
+    const sixty = 60;
+    const twenty_four = 24;
+    let first_date_parse = Date.parse(first_date);
+    let second_date_parse = Date.parse(second_date);
+    let difference = second_date_parse - first_date_parse;
+    let days = difference / (thousand * sixty * sixty * twenty_four);
+    return Math.round(days);
+}
+
+function getAmountOfAdultPeople(data) {
+    const oneYear = 365;
+    const adult = 18;
+    let array = [];
+    for (let i = 0; i < data.length; i++) {
+        const user_birthday = data[i].birthday;
+        const years = daysBetween(user_birthday, new Date()) / oneYear;
+        array.push(years);
+    }
+    const fullYears = filterArray(array, function(el) {
+        return el > adult;
+    });
+    return fullYears.length;
+}
+
+function keys(received_object) {
+    let array = [];
+    for (let prop in received_object) {
+        if ({}.hasOwnProperty.call(received_object, prop)) {
+            array.push(prop);
+        }
+    }
+    return array;
+}
+
+function values(received_object) {
+    let array = [];
+    for (let prop in received_object) {
+        if ({}.hasOwnProperty.call(received_object, prop)) {
+            array.push(received_object[prop]);
+        }
+    }
+    return array;
+}
+
+let task8 = [
+    {
+      '_id': '5b5e3168c6bf40f2c1235cd6',
+      'index': 0,
+      'birthday': '2016-03-18T00:00:00',
+      'eyeColor': 'green',
+      'name': 'Stein',
+      'favoriteFruit': 'apple'
+    },
+    {
+      '_id': '5b5e3168e328c0d72e4f27d8',
+      'index': 1,
+      'birthday': '1991-02-11T00:00:00',
+      'eyeColor': 'blue',
+      'name': 'Cortez',
+      'favoriteFruit': 'strawberry'
+    },
+    {
+      '_id': '5b5e3168cc79132b631c666a',
+      'index': 2,
+      'birthday': '1984-04-17T00:00:00',
+      'eyeColor': 'blue',
+      'name': 'Suzette',
+      'favoriteFruit': 'apple'
+    },
+    {
+      '_id': '5b5e31682093adcc6cd0dde5',
+      'index': 3,
+      'birthday': '1994-04-17T00:00:00',
+      'eyeColor': 'green',
+      'name': 'George',
+      'favoriteFruit': 'banana'
+    }
+]
